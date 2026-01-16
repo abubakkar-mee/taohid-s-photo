ae80b617e64174f97600485085dfca4bcc163907
Abu Bakkar Siddique <abubakkar.mee@gmail.com>
2026-01-16 20:28:08 +0600

    forth release

--- src/App.jsx
+++ src/App.jsx
@@ -4,6 +4,7 @@ import img1 from './assets/img1.jpg'
 import img2 from './assets/img2.jpg'
 import img3 from './assets/img3.jpg'
 import viteLogo from '/vite.svg'
+import { FaSquareFacebook } from "react-icons/fa6";
 
 const images = [img1,img2,img3]
 
@@ -26,14 +27,19 @@ setInterval(changeImg, 2000)
     <>
     <div>
       <h2 className='text-white font-bold text-3xl text-center p-4'>{name}</h2>
-      <div className='flex justify-center mb-8'>
-        <img className='w-[80%] h-[350px] object-cover rounded-xl' src={img1} alt="taohid" />
+      <div className='flex justify-center pb-2 '>
+        <a href="https://web.facebook.com/profile.php?id=61578696069403">
+          <FaSquareFacebook className='text-center text-3xl text-white cursor-pointer'/>
+        </a>
       </div>
       <div className='flex justify-center mb-8'>
-        <img className='w-[80%] h-[350px] object-cover rounded-xl' src={img2} alt="taohid" />
+        <img className='w-[80%] md:w-[50%] h-[350px] object-cover rounded-xl' src={img1} alt="taohid" />
+      </div>
+      <div className='flex justify-center mb-8'>
+        <img className='w-[80%] md:w-[50%] h-[350px] object-cover rounded-xl' src={img2} alt="taohid" />
       </div>
       <div className='flex justify-center'>
-        <img className='w-[80%] h-[350px] object-cover rounded-xl' src={img3} alt="taohid" />
+        <img className='w-[80%] md:w-[50%] h-[350px] object-cover rounded-xl' src={img3} alt="taohid" />
       </div>
 
     </div>
